7a3afe70cb191fa394b16d2fbdb270323cade339
Pablo Assis <pablo.assis1984@gmail.com>
2021-12-12 22:04:10 -0300

    Manipulador de Imagens

---
+++ Manipulador/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Manipulador
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.button1.Location = new System.Drawing.Point(983, 16);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(170, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buscar Arquivos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.Control;
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.button2.Location = new System.Drawing.Point(983, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(170, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Buscar Imagens";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(10, 50);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(275, 604);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.ForeColor = System.Drawing.Color.Navy;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 20;
+            this.listBox2.Location = new System.Drawing.Point(307, 50);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(280, 604);
+            this.listBox2.TabIndex = 3;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Gainsboro;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(593, 118);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(681, 551);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("News706 BT", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Manipulador de Imagens CFTV";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1286, 673);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.Color.Navy;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manipulador CFTV";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private PictureBox pictureBox1;
+        private FolderBrowserDialog folderBrowserDialog1;
+        private Label label1;
+    }
+}
---
+++ Manipulador/Form1.cs
@@ -0,0 +1,115 @@
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+
+namespace Manipulador
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+            
+            
+
+            label1.Text = folderBrowserDialog1.SelectedPath;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath);
+            listBox1.Items.AddRange(files);
+
+            DirectoryInfo dir_files = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            FileInfo[] files2 = dir_files.GetFiles("*", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2)
+            {
+                listBox2.Items.Add(fil.Name);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK) return;
+
+
+
+            label1.Text = folderBrowserDialog1.SelectedPath;
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            string[] files = Directory.GetFiles(folderBrowserDialog1.SelectedPath,"*.bmp");
+            listBox1.Items.AddRange(files);
+            files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.png");
+            listBox1.Items.AddRange(files);
+            files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg");
+            listBox1.Items.AddRange(files);
+            files = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.avi");
+            listBox1.Items.AddRange(files);
+
+
+            DirectoryInfo dir_files = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+            FileInfo[] files2 = dir_files.GetFiles("*", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2)
+            {
+                listBox2.Items.Add(fil.Name);
+            }
+            files2 = dir_files.GetFiles("*.bmp", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2)
+            {
+                listBox2.Items.Add(fil.Name);
+            }
+            files2 = dir_files.GetFiles("*.png", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2)
+            {
+                listBox2.Items.Add(fil.Name);
+            }
+            files2 = dir_files.GetFiles("*.jpg", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2)
+            {
+                listBox2.Items.Add(fil.Name);
+            }
+            files2 = dir_files.GetFiles("*.avi", SearchOption.TopDirectoryOnly);
+            foreach (var fil in files2)
+            {
+                listBox2.Items.Add(fil.Name);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(listBox1.SelectedItem.ToString());
+            }
+            catch (Exception)
+            {  }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             try
+            {
+                pictureBox1.Image = Image.FromFile(folderBrowserDialog1.SelectedPath + "\\" + listBox2.SelectedItem.ToString());
+            }
+            catch (Exception)
+            {  }
+        }
+    }
+}
